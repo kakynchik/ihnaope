4e0df2522ec88b83cefbc92fb81cbe2ca890fa89
student <gudenis234@gmail.com>
2023-04-25 20:51:42 +0300

    завдання №2

--- ihna.py
+++ ihna.py
@@ -1,9 +1,13 @@
-#теорія
-a = 5
-b = 10
-def swap(a, b):
-    a, b = b, a
-    return a, b
-print(f'до обміну: a = {a}, b = {b}')
-a, b = swap(a, b)
-print(f'Після обміну: a = {a}, b = {b}')
+#2
+string = ['hi', 'mene', 'zvaty', 'vova']
+    s = input("введіть текст: ")
+def comit_vowels(s):
+    volwes = "asdadasd"
+    count = 0
+    for leters in s:
+        if leters in volwes:
+            count += 1
+        return count
+#демонстрація
+for s in strings:
+    print(f"у рядку {s} голосних літер {count_vowels(s)}")
